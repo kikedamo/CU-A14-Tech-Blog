bd70348b692f8d0abf26e42c726c2cd95b03ce47
kikedamo <davbaseka@hotmail.com>
2023-07-26 02:53:03 -0400

    ASKBcs

--- seeds/Seed.js
+++ seeds/Seed.js
@@ -3,6 +3,7 @@ const {User,Post} = require('../models');
 const UserData = require('./UserData.json');
 const PostData = require('./PostData.json');
 
+
 const SeedDB = async() =>{
     await sequelize.sync({force:true});
     const User = await User.bulkCreate(UserData, {
